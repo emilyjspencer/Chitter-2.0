c7a6947da163987d9af0bfb1096282d819459c89
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-10-03 20:43:50 +0100

    More hardcoding of friends list

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -27,7 +27,67 @@
         alt=""
       >
 
+      Julia
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Jeni
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Indra
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Caroline
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
       Kat
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Lizzie
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Madeleine
     </div>
   </li>
   <li>
@@ -68,6 +128,26 @@
       >
 
       Jono
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Bonnie M
+    </div>
+  </li>
+  <li>
+    <div class="flex font-bold text-lg mb-4 block">
+      <img 
+        src=""
+        alt=""
+      >
+
+      Bonnie S
     </div>
   </li>
   
